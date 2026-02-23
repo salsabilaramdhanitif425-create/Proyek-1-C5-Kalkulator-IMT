972de0852698387e1ce9f6e51306bb6a972c8630
muhammadfakhritif425-dotcom <muhammad.fakhri.tif425@polban.ac.id>
2026-02-23 15:18:26 +0700

    Rename function tmpilkan_hasil to tampilkan_hasil

--- output.py
+++ output.py
@@ -1,4 +1,4 @@
-def tmpilkan_hasil(gender, berat, tinggi_cm, imt, kategori):
+def tampilkan_hasil(gender, berat, tinggi_cm, imt, kategori):
     
     print ("==========Hasil Perhitungan==========")
     print (f"Gender: {gender}")
@@ -6,4 +6,5 @@ def tmpilkan_hasil(gender, berat, tinggi_cm, imt, kategori):
     print (f"Tinggi Badan: {tinggi_cm} cm")
     print (f"IMT: {imt:.2f}")
     print (f"Kategori: {kategori}")
+
     print("====================================")
