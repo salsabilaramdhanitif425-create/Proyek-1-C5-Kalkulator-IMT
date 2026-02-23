82fddac207e22a5985da26d08a06532aaf81f5b5
salsabilaramdhanitif425-create <salsabila.ramdhani.tif425@polban.ac.id>
2026-02-23 15:33:56 +0700

    Refactor function and variable names for consistency

--- main imt.py
+++ main imt.py
@@ -1,6 +1,5 @@
 from input_data import input_gender, input_berat, input_tinggi
-from converterAndHitungImt import cmTometer
-from hitung_imt import hitung_imt
+from converterAndHitungImt import cmToMeter, hitungImt
 from kategori_IMT import kategori_imt
 from output import tampilkan_hasil
 
@@ -9,12 +8,13 @@ def main():
     Berat = input_berat()
     Tinggi_cm = input_tinggi()
 
-    Tinggi_m = cm_ke_meter(Tinggi_cm)
-    IMT = hitung_imt(Berat, Tinggi_m)
+    Tinggi_m = cmToMeter(Tinggi_cm)
+    IMT = hitungImt(Berat, Tinggi_m)
     Kategori = kategori_imt(IMT, Gender)
 
     tampilkan_hasil(Gender, Berat, Tinggi_cm, IMT, Kategori)
 if __name__ == "__main__":
 
     main()
+
 
